d80128d3e6261f1621b5244a97904385dbee4725
ylovits <ylovits@gmail.com>
2021-03-19 14:50:20 +0100

    Comments and readme

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # maersk
-UI Engineer Coding Exercise
+## UI Engineer Coding Exercise
+
+The page consists of 9 numbered cards
+which can be manipulated by using the
+shuffle and short buttons.
+
+Using JavaScript, it allows the user to:
+
+• Click the shuffle button to randomly
+rearrange the order of the cards
+
+• Click the sort button to place the
+cards in ascending order (1 – 9)
+
+> Vanilla JavaScript.
+
+> No frameworks or libraries used. 
+
+> Not all ES6 features were used (for linting reasons).
+#
+[Click here for a deployed version](https://ylovits.github.io/maersk/)
--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,25 @@
+// generate an array containing the numbers 1-9
 const numArray = [...new Array(9).keys()].map((x) => x + 1);
 
+// Select DOM element containing the cards
 const cards = document.getElementById("cards");
 
+// Takes an array as an argument and renders the cards
 function renderContent(array) {
     cards.innerHTML = array.map(function (number) {
         return `<span class="card card-${number}">${number}</span>`;
     }).join("");
 }
 
+// Render the cards after the array is suffled
 function suffle() {
     renderContent(numArray.sort(() => Math.random() - 0.5));
 }
 
+// Render the cards after the array is sorted
 function sort() {
     renderContent(numArray.sort());
 }
 
+// Render the initian cards
 renderContent(numArray);
